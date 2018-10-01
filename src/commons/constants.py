1f293dd54420186a85a7fb38345be6c0306ee3fe
Utsav Deep <utsavdeep15@gmail.com>
2018-10-01 11:25:05 +0530

    initial project structure

---
+++ src/commons/constants.py
@@ -0,0 +1,4 @@
+RAW_DATA_URL='https://www.kaggle.com/c/bioresponse/download/train.csv'
+RAW_ZIP_FILENAME='all.zip'
+BASE_DIR='/home/utsav/PycharmProjects/kaggle-bio-response'
+KAGGLE_INFO = {'UserName': "utsavdeep15", 'Password': "utsavO(1)"}
